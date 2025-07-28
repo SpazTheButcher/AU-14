eb930aeaa03a02673009b0465fbc6131ddb0180a
Admin <iamevergreendev@gmail.com>
2025-07-28 04:41:41 -0600

    adds coloyantags rule

--- Content.Shared/AU14/PlatoonPrototype.cs
+++ Content.Shared/AU14/PlatoonPrototype.cs
@@ -1,11 +1,10 @@
 using Content.Shared.AU14;
 using Robust.Shared.Prototypes;
 using System.Collections.Generic;
-using Content.Server.Maps;
 using Content.Shared._RMC14.Requisitions;
 using Content.Shared._RMC14.Requisitions.Components;
 
-namespace Content.Server.AU14;
+namespace Content.Shared.AU14;
     [Prototype]
     public sealed partial class PlatoonPrototype : IPrototype
     {
@@ -18,8 +17,8 @@ namespace Content.Server.AU14;
         [DataField("VendorToMarker")]
         public Dictionary<PlatoonMarkerClass, ProtoId<EntityPrototype>> VendorMarkersByClass { get; private set; } = new();
 
-        [DataField("ship")]
-        public ProtoId<GameMapPrototype>? GameMap;
+       // [DataField("ship")]
+        //public ProtoId<GameMapPrototype>? GameMap;
 
       //  [DataField("language")]
        // public ProtoId<LanguagePrototype> Language { get; private set; } = default!;
