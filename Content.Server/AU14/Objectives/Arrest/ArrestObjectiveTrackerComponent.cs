9090422e17a766c21582bd20fa8d144c6148fc69
maertayn <maertayn@cyber-wizard.com>
2026-08-01 22:19:48 +0200

    🔥 remove the old implementation

--- Content.Server/AU14/Objectives/Arrest/ArrestObjectiveTrackerComponent.cs
+++
@@ -1,9 +0,0 @@
-// DEPRECATED: Replaced by _CMU14/Round/Objectives.
-#if false
-
-namespace Content.Server.AU14.Objectives.Arrest;
-
-[RegisterComponent]
-public sealed partial class ArrestObjectiveTrackerComponent : Component;
-
-#endif
