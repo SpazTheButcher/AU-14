04d0d299701af874b199a853f1b80ef5281493a8
TheNewWorkcloxks <jasonknowslots@gmail.com>
2026-08-01 23:05:18 -0700

    Added sentry for Weyland which whitelists everyone + fixes to the map render it works now + stable garrison finally done

---
+++ Content.IntegrationTests/_AU14/Sentry/WeylandYutaniSentryTest.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using Content.Server._RMC14.Sentry;
+using Content.Shared._RMC14.Sentry;
+using Content.Shared._RMC14.Weapons.Ranged.IFF;
+using Robust.Shared.Prototypes;
+
+namespace Content.IntegrationTests._AU14.Sentry;
+
+[TestFixture]
+public sealed class WeylandYutaniSentryTest
+{
+    private static readonly EntProtoId CorporateSentry = "AU14SentryOmniWeYu";
+    private static readonly EntProtoId CorporateNpc = "AU14JobWYPMCPartyContractor";
+    private static readonly EntProtoId<IFFFactionComponent> CorporateIff = "FactionWEYU";
+    private static readonly EntProtoId<IFFFactionComponent> GovforIff = "GOVFOR";
+
+    [Test]
+    public async Task CorporateSentryWhitelistIsLockedAndRecognizesCorporateAffiliates()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
+        var testMap = await pair.CreateTestMap();
+
+        await server.WaitAssertion(() =>
+        {
+            var entities = server.EntMan;
+            var targetingSystem = server.System<SentryTargetingSystem>();
+            var iffSystem = server.System<GunIFFSystem>();
+            var sentry = entities.SpawnEntity(CorporateSentry, testMap.GridCoords);
+            var corporateNpc = entities.SpawnEntity(CorporateNpc, testMap.GridCoords);
+            var corporateIff = entities.SpawnEntity(null, testMap.GridCoords);
+            var outsider = entities.SpawnEntity(null, testMap.GridCoords);
+
+            try
+            {
+                iffSystem.SetUserFaction(corporateIff, CorporateIff);
+                iffSystem.SetUserFaction(outsider, GovforIff);
+
+                var targeting = entities.GetComponent<SentryTargetingComponent>(sentry);
+                var expectedCorporateFactions = new[] { "AUWeYu", "WeYa" };
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(targeting.LockedFriendlyFactions, Is.EquivalentTo(expectedCorporateFactions));
+                    Assert.That(targeting.FriendlyFactions, Is.EquivalentTo(expectedCorporateFactions));
+                    Assert.That(targetingSystem.IsValidTarget((sentry, targeting), corporateNpc), Is.False);
+                    Assert.That(targetingSystem.IsValidTarget((sentry, targeting), corporateIff), Is.False);
+                    Assert.That(targetingSystem.IsValidTarget((sentry, targeting), outsider), Is.True);
+                });
+
+                targetingSystem.SetFriendlyFactions((sentry, targeting), new HashSet<string> { "GOVFOR" });
+                targetingSystem.ToggleFaction((sentry, targeting), "AUWeYu", false);
+                targetingSystem.ClearFactionAssignment((sentry, targeting));
+                targetingSystem.ApplyDeployerFactions(sentry, outsider);
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(targeting.FriendlyFactions, Is.EquivalentTo(expectedCorporateFactions));
+                    Assert.That(targeting.DeployedFriendlyFactions, Is.EquivalentTo(expectedCorporateFactions));
+                    Assert.That(targeting.AllianceFriendlyNpcFactions, Is.Empty);
+                });
+            }
+            finally
+            {
+                entities.DeleteEntity(sentry);
+                entities.DeleteEntity(corporateNpc);
+                entities.DeleteEntity(corporateIff);
+                entities.DeleteEntity(outsider);
+            }
+        });
+
+        await pair.CleanReturnAsync();
+    }
+}
--- Content.Shared/_RMC14/Sentry/SentryTargettingComponent.cs
+++ Content.Shared/_RMC14/Sentry/SentryTargettingComponent.cs
@@ -11,6 +11,13 @@ public sealed partial class SentryTargetingComponent : Component
     [DataField, AutoNetworkedField]
     public HashSet<string> FriendlyFactions = new();
 
+    /// <summary>
+    /// If populated, this sentry always uses exactly these friendly factions.
+    /// Deployer assignment, multitools, and sentry laptops cannot replace this whitelist.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public HashSet<string> LockedFriendlyFactions = new();
+
     [DataField, AutoNetworkedField]
     public HashSet<string> DeployedFriendlyFactions = new();
 
