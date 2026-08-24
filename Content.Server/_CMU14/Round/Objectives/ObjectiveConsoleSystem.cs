b3f7f616e6f72a957e52cb0e1284330f7ebfffa6
maertayn <maertayn@cyber-wizard.com>
2026-08-24 02:24:59 +0200

    🩹 add a Repeating status to the objectives console

--- Content.Server/_CMU14/Round/Objectives/ObjectiveConsoleSystem.cs
+++ Content.Server/_CMU14/Round/Objectives/ObjectiveConsoleSystem.cs
@@ -185,6 +185,10 @@ public sealed partial class ObjectiveConsoleSystem : SharedObjectiveConsoleSyste
             {
                 statusDisplay = ObjectiveStatusDisplay.Uncompleted;
             }
+            if (statusDisplay == ObjectiveStatusDisplay.Uncompleted
+                && objComp.Repeating
+                && objComp.TimesCompleted > 0)
+                statusDisplay = ObjectiveStatusDisplay.Repeating;
             ObjectiveTypeDisplay typeDisplay;
             if (objComp.ObjectiveLevel == 3)
                 typeDisplay = ObjectiveTypeDisplay.Win;
